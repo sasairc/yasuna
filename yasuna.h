afa9cb0b0e7bd4239650081a5b9e79bf0b71f916
sasairc <sasairc@ssiserver.moe.hm>
2015-01-21 17:43:35 +0900

    yasuna.hも上記通り修正

--- yasuna.h
+++ yasuna.h
@@ -1,12 +1,22 @@
+/*
+ * yasuna - Yasuna Oribe will talk.
+ *
+ * Copyright (c) 2015 sasairc
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar.HocevarHocevar See the COPYING file or http://www.wtfpl.net/
+ * for more details.
+ */
+
 #ifndef _YASUNA_H
 #define _YASUNA_H
 
 #include <stdio.h>
 
-#define PROGNAME        "yasuna"
-//#define       MONO            /* パスを組み込みたくないときにお使い下さい */
-#define VERSION         "0.1"
-#define BUFLEN          512
+#define PROGNAME    "yasuna"
+//#define MONO      /* パスを組み込みたくないときにお使い下さい */
+#define VERSION     "0.1"
+#define BUFLEN      512
 
 char** malloc2d(int x, int y);
 int init2d(char** buf, int x, int y);
