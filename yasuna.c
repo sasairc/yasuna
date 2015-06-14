8b4af16705099159ba44b6ef72810ea100b9be92
sasairc <sasairc@ssiserver.moe.hm>
2015-06-14 10:24:23 +0900

    -n指定した時の比較間違えてた・・・

--- yasuna.c
+++ yasuna.c
@@ -124,7 +124,7 @@ int main(int argc, char* argv[])
             point = create_rand(lines);         /* get pseudo-random nuber */
         } while (buf[point] == NULL);
     } else {
-        if (lines >= yasuna.narg)   point = yasuna.narg;
+        if (lines > yasuna.narg)    point = yasuna.narg;
     }
 
     fprintf(stdout, "%s\n", buf[point]);        /* print of string */
