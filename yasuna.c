39d13ff173d523243f203cdef0f7157a968af712
sasairc <sasairc@ssiserver.moe.hm>
2015-07-05 08:22:12 +0900

    print_version()及びprint_usage()を削除　代わりにinfo.hをインクルードするよう追加

--- yasuna.c
+++ yasuna.c
@@ -12,6 +12,7 @@
 
 #include "./config.h"
 #include "./yasuna.h"
+#include "./info.h"
 #include "./subset.h"
 #include "./string.h"
 #include "./file.h"
@@ -132,33 +133,4 @@ void release(FILE* fp, char* path, int lines, char** buf)
     }
 
     return;
-}
-
-int print_version(void)
-{
-    fprintf(stdout, "%s %d.%d.%d (%s)\n", PROGNAME, VERSION, PATCHLEVEL, SUBLEVEL, ARCH);
-
-    exit(0);
-}
-
-int print_usage(void)
-{
-    fprintf(stdout, "\
-%s %d.%d.%d, Yasuna Oribe will talk.\n\
-Usage: %s [OPTION]...\n\
-\n\
-Mandatory arguments to long options are mandatory for short options too.\n\
-\n\
-  -l,  --list                print all quotes list and exit\n\
-  -n,  --number INT          specify quote number\n\
-  -f,  --file=PATH           specfiles the dictionary file\n\
-\n\
-  -h,  --help                display this help and exit\n\
-  -v,  --version             output version infomation and exit\n\
-\n\
-Report %s bugs to %s <%s>\n\
-", PROGNAME, VERSION, PATCHLEVEL, SUBLEVEL,
-PROGNAME, PROGNAME, AUTHOR, MAIL_TO);
-
-    exit(0);
 }
