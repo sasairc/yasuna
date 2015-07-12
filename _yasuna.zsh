3cc603f108302885d0516b80f0282af5a631c57b
sasairc <sasairc@ssiserver.moe.hm>
2015-07-12 10:42:12 +0900

    やすなちゃんの補完関数

---
+++ _yasuna.zsh
@@ -0,0 +1,12 @@
+#compdef yasuna
+
+function _yasuna() {
+    _arguments -s : \
+        {-l,--list}'[print all quotes list and exit]' \
+        {-n,--number}'[specify quote number]':keyword \
+        {-f,--file}'[specfiles the dictionary file]:file:_files' \
+        {-h,--help}'[display help]' \
+        {-v,--version}'[output version]'
+}
+
+_yasuna
