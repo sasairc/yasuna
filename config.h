11c7274fa23eadcad013defb62526a4429bc2283
sasairc <sasairc@ssiserver.moe.hm>
2015-07-15 23:08:43 +0900

    yasuna 7.2.4-devel (x86_64-linux-gnu)

--- config.h
+++ config.h
@@ -16,8 +16,8 @@
 #define PROGNAME        "yasuna"
 #define VERSION         7
 #define PATCHLEVEL      2
-#define SUBLEVEL        3
-#define EXTRAVERSION    ""
+#define SUBLEVEL        4
+#define EXTRAVERSION    "-devel"
 
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
