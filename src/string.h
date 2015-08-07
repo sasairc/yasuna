aaf86f54f410d2295577efc043c319a900cb76f6
sasairc <sasairc@ssiserver.moe.hm>
2015-08-08 00:36:45 +0900

    ソース類をsrc下に分割

---
+++ src/string.h
@@ -0,0 +1,30 @@
+/*
+ * yasuna - Yasuna Oribe will talk.
+ *
+ * string.h
+ *
+ * Copyright (c) 2015 sasairc
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar.HocevarHocevar See the COPYING file or http://www.wtfpl.net/
+ * for more details.
+ */
+
+
+#ifndef YSTRING_H
+#define YSTRING_H
+
+#undef  DEBUG
+#undef  WITH_GLIB   /* use glib */
+#define LOCALE  "ja_JP.UTF-8"
+
+/* This functions is required string.c */
+extern int strrep(char* src, char* haystack, char* needle);
+extern char* strlion(int argnum, ...);
+extern int mbstrlen(char* src);
+extern int strunesc(char* src);
+extern int strmax(int val, char** src);
+extern int strlftonull(char* str);
+extern char** str_to_args(char* str);
+
+#endif
---
+++ src/yasuna.h
@@ -0,0 +1,30 @@
+/*
+ * yasuna - Yasuna Oribe will talk.
+ *
+ * yasuna.h
+ * 
+ * Copyright (c) 2015 sasairc
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar.HocevarHocevar See the COPYING file or http://www.wtfpl.net/
+ * for more details.
+ */
+
+#ifndef _YASUNA_H
+#define _YASUNA_H
+
+#include <stdio.h>
+
+typedef struct YASUNA_T {
+    int lflag;      /* List flag(--list). */
+    int fflag;      /* Dictionary flag(--dict=PATH). this flag use getopt_long() */
+    int nflag;      /* Number flag(--number=INT). this flag use getopt_long() */
+    int narg;       /* Number arguments(--number=INT). this val use getopt_long() */
+    char* farg;     /* Dictionary arguments(--dict=PATH). this val use getopt_long()  */
+} yasuna_t;
+
+extern void release(FILE* fp, char* path, int lines, char** buf);
+extern int print_version(void);
+extern int print_usage(void);
+
+#endif
