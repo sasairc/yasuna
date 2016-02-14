164e72815a3f583ca600c14c020f66b66bdd890a
sasairc <sasairc@ssiserver.moe.hm>
2016-02-14 20:14:49 +0900

    libpolyanessの更新

--- src/polyaness.c
+++ src/polyaness.c
@@ -37,6 +37,9 @@
 int count_keys(const char* str);
 int add_data_polyaness(int record, int keys, const char* str, polyaness_t*** polyaness);
 
+/* misc functions */
+static int strcmp_lite(const char* str1, const char* str2);
+
 int init_polyaness(FILE* fp, int offset, polyaness_t** polyaness)
 {
     if (fp == NULL)
@@ -232,17 +235,13 @@ char* get_polyaness(const char* key, int record, polyaness_t** polyaness)
 
     j = (*polyaness)->record[record]->keys - 1;
     while (i < (*polyaness)->record[record]->keys && i <= j) {
-        if ((*polyaness)->record[record]->key[i] != NULL) {
-            if (strcmp((*polyaness)->record[record]->key[i], key) == 0) {
-                match = (*polyaness)->record[record]->value[i];
-                break;
-            }
+        if (strcmp_lite((*polyaness)->record[record]->key[i], key) == 0) {
+            match = (*polyaness)->record[record]->value[i];
+            break;
         }
-        if ((*polyaness)->record[record]->key[j] != NULL) {
-            if (strcmp((*polyaness)->record[record]->key[j], key) == 0) {
-                match = (*polyaness)->record[record]->value[j];
-                break;
-            }
+        if (strcmp_lite((*polyaness)->record[record]->key[j], key) == 0) {
+            match = (*polyaness)->record[record]->value[j];
+            break;
         }
         i++;
         j--;
@@ -285,4 +284,32 @@ void release_polyaness(polyaness_t* polyaness)
     polyaness = NULL;
 
     return;
+}
+
+/*
+ * misc functions
+ */
+
+static int strcmp_lite(const char* str1, const char* str2)
+{
+    if (str1 == NULL || str2 == NULL)
+        return -1;
+
+    int     cnt     = 0;
+
+    size_t  len1    = 0,
+            len2    = 0;
+
+    len1 = strlen(str1);
+    len2 = strlen(str2);
+
+    while (*str1 == *str2 && *str1 != '\0' && *str2 != '\0') {
+        str1++;
+        str2++;
+        cnt++;
+    }
+    if (cnt == len1 && cnt == len2)
+        return 0;
+
+    return 1;
 }
