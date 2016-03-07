e1467c984d271f37deef5200e32d7f10b85db5b6
sasairc <sasairc@ssiserver.moe.hm>
2016-03-08 05:44:20 +0900

    yasuna 7.4.3-devel (x86_64-linux-gnu)

--- src/config.h
+++ src/config.h
@@ -16,8 +16,8 @@
 #define PROGNAME        "yasuna"
 #define VERSION         7
 #define PATCHLEVEL      4
-#define SUBLEVEL        2
-#define EXTRAVERSION    ""
+#define SUBLEVEL        3
+#define EXTRAVERSION    "-devel"
 
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
