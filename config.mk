110284573d76cf96f532603bc2bf86dfd866b518
sasairc <sasairc@ssiserver.moe.hm>
2017-08-20 16:05:10 +0900

    modified:   config.mk 	modified:   quotes/Makefile 	modified:   src/Makefile

--- config.mk
+++ config.mk
@@ -2,9 +2,8 @@
 #    Makefile for yasuna
 #
 
-WITH_SHARED = 0
-DICNME      = yasuna-quotes
-# DICNME = yasuna-quotes-lite
-BUFLEN      = 4096
-TH_LINES    = 1024
-TH_LENGTH   = 640
+WITH_SHARED	= 0
+DICNME		=
+BUFLEN		=
+TH_LINES	=
+TH_LENGTH	=
