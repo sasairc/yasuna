6e4b4339d271282aa0aaceb33a7f9faa90ab5f81
sasairc <sasairc@ssiserver.moe.hm>
2021-03-26 20:10:36 +0900

    yasuna 12.7.0-devel (x86_64-linux-gnu)

--- src/config.h
+++ src/config.h
@@ -18,10 +18,10 @@ extern "C" {
 #endif
 
 #define PROGNAME        "yasuna"
-#define VERSION         11
+#define VERSION         12
 #define PATCHLEVEL      7
 #define SUBLEVEL        0
-#define EXTRAVERSION    ""
+#define EXTRAVERSION    "-devel"
 
 #define AUTHOR          "sasairc"
 #define MAIL_TO         "sasairc@ssiserver.moe.hm"
